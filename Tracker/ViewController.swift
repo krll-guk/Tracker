af0ee28e447e649882c5034ddb0ac8d11cb35188
krll-guk <krll.guk@yandex.ru>
2023-04-30 14:29:59 +0300

    setup

--- Tracker/ViewController.swift
+++ Tracker/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  Tracker
-//
-//  Created by Kirill Guk on 30/4/23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
